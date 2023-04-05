a9ab1311b4932f8e00d6e78d175614a5e4913757
Alex S <i.alexandr.sh@gmail.com>
2023-04-05 17:45:45 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	docker compose run --rm app npm install
+
+start:
+	docker compose run --rm app npm start
---
+++ main.ts
@@ -0,0 +1,7 @@
+import { OpenAI } from "langchain";
+
+const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9 });
+const res = await model.call(
+    "What would be a good company name a company that makes colorful socks?"
+  );
+  console.log(res);
